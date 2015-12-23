e59755f60a3c83224a31bcc0de3cf1ae1f4ee996
Priit Pirita <atirip@gmail.com>
2015-12-23 11:32:45 +0200

    - allow function as property everywhere - pass vars as function argument for all function calls - fix require

--- ptemplayed.js
+++ ptemplayed.js
@@ -4,24 +4,34 @@
 /* (c) Paul Engel (Internetbureau Holder B.V.)
 */
 
+/*jshint laxcomma:true, laxbreak: true, asi:true */
 ;(function() {
 
 	function templayed (template, vars) {
-		var get = function(path, i) {
-				i = 1
+		var get = function(path) {
+				var i = 1
 				path = path.replace(/\.\.\//g, function() { i++; return ''; })
 				var js = ['vars[vars.length - ', i, ']']
 				var keys = (path == "." ? [] : path.split("."))
 				var j = 0
 				for (; j < keys.length; j++) js.push('.' + keys[j])
-				return js.join('')
+				/*
+					- allow function as property everywhere
+					- pass vars as function argument for all function calls
+						var foo = {
+							bar: function(blaah) {
+								console.log(blaah === foo); // true
+							}
+						}
+				*/
+				return '(function(x) {return x&&x.call ? x.call(vars[vars.length - 1], vars[0]) : x;})('+js.join('')+')'
 			}
 
 		var tag = function(template) {
 			return template.replace(/\{\{(!|&|\{)?\s*(.*?)\s*}}+/g, function(match, operator, context) {
 				if (operator == "!") return ''
 				var i = inc++
-				return ['"; var o', i, ' = ', get(context), ', s', i, ' = typeof(o', i, ') == "function" ? o', i, '.call(vars[vars.length - 1]) : o', i, '; s', i,' = ( s', i,' || s', i,' == 0 ? s', i,': "") + ""; s += ',
+				return ['"; var s', i, ' = ', get(context), '; s', i,' = ( s', i,' || s', i,' == 0 ? s', i,': "") + ""; s += ',
 					(operator ? ('s' + i) : '(/[&"><]/.test(s' + i + ') ? s' + i + '.replace(/&/g,"&amp;").replace(/"/g,"&quot;").replace(/>/g,"&gt;").replace(/</g,"&lt;") : s' + i + ')'), ' + "'].join('')
 				})
 			}
@@ -32,6 +42,7 @@
 				return ['"; var o', i, ' = ', get(key), '; ', (
 					operator == "^"
 					?	['if ((o', i, ' instanceof Array) ? !o', i, '.length : !o', i, ') { s += "', block(context), '"; } ']
+					// use truthy/falsy for # ^|#
 					:	['if ( !(o', i, ' instanceof Array) && o', i, ') { s += "', block(context), '"; } else if (o', i, ') { for (var i', i, ' = 0; i', i, ' < o',
 						i, '.length; i', i, '++) { vars.push(o', i, '[i', i, ']); s += "', block(context), '"; vars.pop(); }}']
 				).join(''), '; s += "'].join('')
@@ -41,15 +52,16 @@
 		var inc = 0
 
 		return new Function("vars", 'vars = [vars]; var s = "' + block(template.replace(/"/g, '\\"').replace(/\n/g, '\\n')) + '"; return s;');
-
 	}
 
 	if (typeof define === 'function' && define.amd) {
-		define(templayed)
+		define(function() {
+			return templayed;
+		});
 	} else if (typeof module !== 'undefined' && module.exports) {
 		module.exports = templayed
 	} else {
 		this.ptemplayed = templayed
 	}
 
-}).call(this);		
+}).call(this);
